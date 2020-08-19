369f087bb52531b6c6da5f22b1401a1a828ce6d4
AndresParraGO <andresparragn@gmail.com>
2020-08-19 10:25:40 -0500

    add dark mode

---
+++ src/components/DarkMode.js
@@ -0,0 +1,26 @@
+
+import React, {useState} from 'react';
+import darkmode from '../assets/darkmode.png';
+
+function DarkMode() {
+	const [darkMode, setDarkMode] = useState(false)
+
+	function changeMode() {
+		if(darkMode) {
+			document.body.classList.add('dark-mode-colors')
+		} else {
+			document.body.classList.remove('dark-mode-colors')
+		}
+	}
+
+	return (
+		<div onClick={() => {
+			setDarkMode(!darkMode)
+			changeMode()
+		}} className="dark__mode">
+			<img className="dark__mode-icon" src={darkmode} />
+		</div>
+	)
+}
+
+export default DarkMode
