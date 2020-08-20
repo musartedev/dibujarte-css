2ba95f659a978ae1514e5d5487449cac47cb8236
AndresParraGO <andresparragn@gmail.com>
2020-08-19 21:40:32 -0500

    format spaces to 2

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -3,24 +3,24 @@ import React, {useState} from 'react';
 import darkmode from '../assets/darkmode.png';
 
 function DarkMode() {
-	const [darkMode, setDarkMode] = useState(false)
+	const [darkMode, setDarkMode] = useState(false);
 
 	function changeMode() {
 		if(darkMode) {
-			document.body.classList.add('dark-mode-colors')
+			document.body.classList.add('dark-mode-colors');
 		} else {
-			document.body.classList.remove('dark-mode-colors')
+			document.body.classList.remove('dark-mode-colors');
 		}
 	}
 
 	return (
 		<div onClick={() => {
-			setDarkMode(!darkMode)
-			changeMode()
+			setDarkMode(!darkMode);
+			changeMode();
 		}} className="dark__mode">
 			<img className="dark__mode-icon" src={darkmode} />
 		</div>
 	)
 }
 
-export default DarkMode
+export default DarkMode;
