4ccfd058b4b1893df1bbcc611befffc709f94e89
AndresParraGO <andresparragn@gmail.com>
2020-08-19 21:51:02 -0500

    add icon lightmode

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -1,15 +1,20 @@
 
 import React, {useState} from 'react';
 import darkmode from '../assets/darkmode.png';
+import lightmode from '../assets/light.png';
+
 
 function DarkMode() {
 	const [darkMode, setDarkMode] = useState(false);
+  const [img, setImg] = useState(darkmode);
 
 	function changeMode() {
 		if(darkMode) {
 			document.body.classList.add('dark-mode-colors');
+      setImg(lightmode);
 		} else {
 			document.body.classList.remove('dark-mode-colors');
+      setImg(darkmode);
 		}
 	}
 
@@ -18,7 +23,7 @@ function DarkMode() {
 			setDarkMode(!darkMode);
 			changeMode();
 		}} className="dark__mode">
-			<img className="dark__mode-icon" src={darkmode} />
+			<img className="dark__mode-icon" src={img} />
 		</div>
 	)
 }
