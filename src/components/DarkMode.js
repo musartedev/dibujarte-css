48c1a85cedb12d76e592dbaa93173a8b1e61b754
tuff <aldorl.mee@gmail.com>
2020-08-23 12:44:58 +0000

    [FIX] scroll and dark mode

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -5,24 +5,19 @@ import lightmode from '../assets/light.png';
 
 
 function DarkMode() {
-	const [darkMode, setDarkMode] = useState(false);
   const [img, setImg] = useState(darkmode);
 
-	function changeMode() {
-		if(darkMode) {
-			document.body.classList.add('dark-mode-colors');
-      setImg(lightmode);
-		} else {
-			document.body.classList.remove('dark-mode-colors');
-      setImg(darkmode);
-		}
-	}
+  const handlerDarkMode = () => {
+    const body = document.body
+    const isDarkMode = body.classList.contains('dark-mode-colors')
+    setImg(
+      isDarkMode ?darkmode :lightmode
+    )
+    body.classList.toggle('dark-mode-colors');
+  }
 
 	return (
-		<div onClick={() => {
-			setDarkMode(!darkMode);
-			changeMode();
-		}} className="dark__mode">
+		<div onClick={handlerDarkMode} className="dark__mode">
 			<img className="dark__mode-icon" src={img} alt="dark mode icon" />
 		</div>
 	)
